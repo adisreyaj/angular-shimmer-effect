e83e8aa42b350e7b2b4fe07376c133318a452fa4
Adithya Sreyaj <adi.sreyaj@gmail.com>
2019-12-08 22:56:37 +0530

    Corrected the import statement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShimmerModule } from 'ng-shimmer';
+import { ShimmerModule } from '@sreyaj/ng-shimmer';
 import { UserRatingComponent } from './cards/user-rating/user-rating.component';
 import { FoodRatingComponent } from './cards/food-rating/food-rating.component';
 import { StarRatingModule } from '@sreyaj/ng-star-rating';
